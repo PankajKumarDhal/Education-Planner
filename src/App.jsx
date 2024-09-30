8117ad1eee18b21932c3a06c359b7e925a703b21
Pankaj Kumar Dhal <pk99dhal@gmail.com>
2024-09-30 15:31:25 +0530

    edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import styles from "./App.module.css";
-import "./App.css";
+// import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const [subjects, setSubjects] = useState([]);
   const [newSubject, setNewSubject] = useState("");
   const [newHours, setNewHours] = useState("");
